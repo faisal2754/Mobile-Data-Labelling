de22bd9a5a3601e68531d1e0bf21b5cba5cd955f
Faisal <saleemfs27@gmail.com>
2021-06-29 13:45:22 +0200

    Network request & simple navigation

--- App.js
+++ App.js
@@ -1,28 +1,26 @@
+import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, TextInput, View, Image, Alert } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
-import Button from './components/Button'
+import Button from './components/loginButton'
+
+const Stack = createStackNavigator()
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <StatusBar style="auto" />
-            <View style={{}}>
-                <Image
-                    source={require('./assets/mountains.jpg')}
-                    style={styles.logo}
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{ headerMode: 'none', headerShown: false }}
                 />
-            </View>
-            <View style={{ paddingTop: 50, alignItems: 'center' }}>
-                <TextInput style={styles.input} placeholder="Email" />
-                <TextInput style={styles.input} placeholder="Password" />
-                <Button
-                    title="Log In"
-                    onPress={() => Alert.alert('Simple Button pressed')}
-                />
-            </View>
-        </View>
+                <Stack.Screen name="main" component={main} />
+            </Stack.Navigator>
+        </NavigationContainer>
     )
 }
 
@@ -48,3 +46,54 @@ const styles = StyleSheet.create({
         borderRadius: 500
     }
 })
+
+function HomeScreen({ navigation }) {
+    const [isLoading, setLoading] = useState(true)
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch(
+            'https://raw.githubusercontent.com/adhithiravi/React-Hooks-Examples/master/testAPI.json'
+        )
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false))
+    }, [])
+
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+
+            {/* Logo */}
+            <View style={{}}>
+                <Image
+                    source={require('./assets/mountains.jpg')}
+                    style={styles.logo}
+                />
+            </View>
+
+            {/* Input text & Button */}
+            <View style={{ paddingTop: 50, alignItems: 'center' }}>
+                <TextInput style={styles.input} placeholder={data.title} />
+                <TextInput style={styles.input} placeholder="Password" />
+                <Button
+                    title="Log In"
+                    onPress={() => navigation.navigate('main')}
+                />
+            </View>
+        </View>
+    )
+}
+
+function main({ navigation }) {
+    return (
+        <View style={styles.container}>
+            {/* <Button
+                title="Bruh"
+                onPress={() => navigation.navigate('main', { name: 'Faisal' })}
+            /> */}
+            <Text>Big bruh moment</Text>
+        </View>
+    )
+}
