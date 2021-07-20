8622bcc230cf25c94f6e8be69f2de26ba057ff62
Faisal <saleemfs27@gmail.com>
2021-07-20 13:41:31 +0200

    Updated mongoose connection method

--- server/server.js
+++ server/server.js
@@ -3,34 +3,20 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const { typeDefs, resolvers } = require('./schema/schema')
 
-async function connectDB() {
-   await mongoose.connect(
-      process.env.DB_CONNECT,
-      {
-         useUnifiedTopology: true,
-         useNewUrlParser: true,
-         useFindAndModify: false,
-         useCreateIndex: true
-      },
-      () => {
-         console.log('connected to db!')
-      }
-   )
-}
+const conn = mongoose.createConnection(
+   process.env.DB_CONNECT,
+   {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+   },
+   () => console.log('Connected to db')
+)
 
-//safety starts
-mongoose.connection.on('error', (err) => {
-   console.log(err)
-})
-//end of safety
+const server = new ApolloServer({ typeDefs, resolvers, conn })
 
-connectDB()
-const connection = mongoose.connection
-
-// console.log(connection)
-
-const server = new ApolloServer({ typeDefs, resolvers, connection })
-
+// The `listen` method launches a web server.
 server.listen().then(({ url }) => {
    console.log(`🚀  Server ready at ${url}`)
 })
